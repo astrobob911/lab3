7bc080052dbbd525e76672ccf4f487d94d98c506
astrobob911 <73851363+astrobob911@users.noreply.github.com>
2023-03-13 08:54:09 -0400

    Initial commit

---
+++ load_data.py
@@ -0,0 +1,34 @@
+# ECOR 1042 Lab 3 - Team submission
+# Remember to include docstring and type annotations for your functions
+
+# Update "" to list all students contributing to the team work
+__author__ = ""
+
+# Update "" with your team (e.g. T102)
+__team__ = ""
+
+#==========================================#
+# Place your student_school_list function after this line
+
+
+#==========================================#
+# Place your student_health_list function after this line
+
+
+#==========================================#
+# Place your student_age_list function after this line
+
+
+#==========================================#
+# Place your student_failures_list function after this line
+
+
+#==========================================#
+# Place your load_data function after this line
+
+
+#==========================================#
+# Place your add_average function after this line
+
+
+# Do NOT include a main script in your submission
